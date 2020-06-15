ac22c17ca87df363665b7720acef8413aa7d25af
Adriane Inocencio <adriane.0523@gmail.com>
2020-06-15 11:19:52 -0700

    fixed allowed hosts

--- virtuallearninglab/settings.py
+++ virtuallearninglab/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '0ywxb*nsks*$_3yjvj9im4bot&c^5%=+jygrgoe3x%%xuh_g65'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['virtual-learning-lab.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['virtual-learning-lab.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
